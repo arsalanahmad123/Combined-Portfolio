da5f0688e5abbdb8d3d3f3c564be99b1a12252be
arsalanahmad123 <arsalanahmad15092003@gmail.com>
2024-03-21 16:30:03 +0500

    added image

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,47 +1,48 @@
 import { motion } from 'framer-motion'
 import React from 'react'
-import phone from '../assets/phone.png'
+import earth from '../assets/earth.png'
+import formImage from '../assets/form2.jpg'
 
 const Contact = () => {
     return (
         <>
-            <section className='min-h-screen w-full flex justify-center items-center relative'>
-                <div className='w-full h-full flex lg:flex-row flex-col justify-between items-center gap-x-5 md:px-20 px-5'>
-                    <motion.div
-                        className='flex flex-col gap-y-5 p-10 bg-white  rounded-lg '
-                        initial={{ opacity: 0, y: 100 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        exit={{ opacity: 0, y: 100 }}
-                        whileHover={{ boxShadow: '0 0 10px #1a2c3a' }}
-                        transition={{ duration: 0.5, ease: 'easeInOut' }}
-                    >
-                        <h1 className='text-gray-800 text-5xl font-bold monster'>
+            <section
+                className='min-h-screen mt-44
+             w-full flex justify-center items-center relative overflow-hidden'
+            >
+                <div className='w-full h-full absolute top-0 left-0 -z-10'>
+                    <img src={formImage} className='w-full h-[1000px]' alt='' />
+                </div>
+                <div className='w-full h-full  gap-x-5 md:px-20 px-5 gap-y-5 mt-[520px]'>
+                    <div className='flex flex-col gap-y-5 p-10 rounded-lg w-full'>
+                        <h1 className='text-gray-800 text-7xl font-bold monster'>
                             GET IN TOUCH
                         </h1>
-                        <form className='flex flex-col poppins'>
-                            <input
-                                type='text'
-                                placeholder='Name'
-                                className='bg-[#1a2c3a] text-white w-full p-3 my-2 rounded-md placeholder:text-white'
-                            />
-                            <input
-                                type='email'
-                                placeholder='Email'
-                                className='bg-[#1a2c3a] text-white w-full p-3 my-2 rounded-md placeholder:text-white'
-                            />
-                            <textarea
-                                className='bg-[#1a2c3a] text-white w-full p-3 my-2 rounded-md placeholder:text-white'
-                                placeholder='Message'
-                            ></textarea>
-                            <button
-                                type='submit'
-                                className='bg-[#1a2c3a] text-white w-full p-3 my-2 rounded-md placeholder:text-white'
-                            >
-                                SEND
-                            </button>
-                        </form>
-                    </motion.div>
-                    <img src={phone} alt='' />
+                        <div>
+                            <form className='flex flex-col poppins'>
+                                <input
+                                    type='text'
+                                    placeholder='Name'
+                                    className='bg-[#1a2c3a] text-white w-full p-3 my-2 rounded-md placeholder:text-white'
+                                />
+                                <input
+                                    type='email'
+                                    placeholder='Email'
+                                    className='bg-[#1a2c3a] text-white w-full p-3 my-2 rounded-md placeholder:text-white'
+                                />
+                                <textarea
+                                    className='bg-[#1a2c3a] text-white w-full p-3 my-2 rounded-md placeholder:text-white'
+                                    placeholder='Message'
+                                ></textarea>
+                                <button
+                                    type='submit'
+                                    className='bg-[#1a2c3a] text-white w-full p-3 my-2 rounded-md placeholder:text-white'
+                                >
+                                    SEND
+                                </button>
+                            </form>
+                        </div>
+                    </div>
                 </div>
             </section>
         </>
--- src/components/SelectedWork.jsx
+++ src/components/SelectedWork.jsx
@@ -2,19 +2,6 @@ import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 import solution from '../assets/solution.png'
 const SelectedWork = () => {
-    const [projects, setProjects] = useState([
-        {
-            name: 'Project 1',
-            image: 'https://images.unsplash.com/photo-1526779259212-939e64788e3c?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        },
-        {
-            name: 'Project 2',
-            image: 'https://images.unsplash.com/photo-1593696954577-ab3d39317b97?q=80&w=1466&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        },
-    ])
-
-    const [selectedProject, setSelectedProject] = useState(projects[0])
-
     return (
         <>
             <img src={solution} alt='' />
@@ -25,8 +12,13 @@ const SelectedWork = () => {
                 <h1 className='text-4xl md:text-7xl font-semibold tracking-widest poppins title whitespace-nowrap  shadow-text'>
                     Selected Work
                 </h1>
-                <motion.div className='grid grid-cols-1 md:grid-cols-3 grid-rows-2 w-full gap-10  poppins'>
-                    <div className='flex flex-col justify-start items-start gap-y-5 bg-white  rounded-lg shadow-2xl max-h-[540px]'>
+                <div className='grid grid-cols-1 md:grid-cols-3 grid-rows-2 w-full gap-10  poppins'>
+                    <motion.div
+                        className='flex flex-col justify-start items-start gap-y-5 bg-white  rounded-lg shadow-2xl max-h-[540px]'
+                        whileHover={{
+                            scale: 0.95,
+                        }}
+                    >
                         <img
                             src='https://images.unsplash.com/photo-1526779259212-939e64788e3c'
                             alt=''
@@ -40,8 +32,13 @@ const SelectedWork = () => {
                         <button className='text-base mx-4 px-2 py-1 bg-[#1a2c3a] text-white'>
                             View Project
                         </button>
-                    </div>
-                    <div className=' bg-white grid grid-rows-4 rounded-lg p-5 gap-y-3 col-span-2 shadow-2xl shadow-[#1a2c3a]'>
+                    </motion.div>
+                    <motion.div
+                        className=' bg-white grid grid-rows-4 rounded-lg p-5 gap-y-3 col-span-2 shadow-2xl shadow-[#1a2c3a]'
+                        whileHover={{
+                            scale: 0.95,
+                        }}
+                    >
                         <div className='flex flex-row justify-start items-star bg-white rounded-lg row-span-2 gap-x-4'>
                             <img
                                 src='https://images.unsplash.com/photo-1526779259212-939e64788e3c'
@@ -78,8 +75,13 @@ const SelectedWork = () => {
                                 </button>
                             </div>
                         </div>
-                    </div>
-                    <div className='flex flex-row justify-start items-start gap-x-4 col-span-3 row-span-4 bg-white rounded-lg max-h-[500px] shadow-2xl shadow-[#6d86ad]'>
+                    </motion.div>
+                    <motion.div
+                        className='flex flex-row justify-start items-start gap-x-4 col-span-3 row-span-4 bg-white rounded-lg max-h-[500px] shadow-2xl shadow-[#6d86ad]'
+                        whileHover={{
+                            scale: 0.95,
+                        }}
+                    >
                         <img
                             src='https://images.unsplash.com/photo-1526779259212-939e64788e3c'
                             className='w-1/2 h-full'
@@ -96,8 +98,8 @@ const SelectedWork = () => {
                                 View Project
                             </button>
                         </div>
-                    </div>
-                </motion.div>
+                    </motion.div>
+                </div>
             </section>
         </>
     )
